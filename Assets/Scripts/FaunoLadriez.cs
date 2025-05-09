0fd776f64dde423ddec9975b477dc39991bdfb23
JaviFigueroaVicente <javierfigueroa969@gmail.com>
2025-05-09 17:36:54 +0200

    Fauno vida

--- Assets/Scripts/FaunoLadriez.cs
+++ Assets/Scripts/FaunoLadriez.cs
@@ -1,7 +1,10 @@
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using TMPro;
 
 public class FaunoLadriez : CharacterController{
+    public int vida = 1;
+    public TextMeshProUGUI gameOverText;
      protected override void Update()
     {
         float inputMoviminetoHorizontal = Input.GetAxis("Horizontal");
@@ -9,5 +12,23 @@ public class FaunoLadriez : CharacterController{
 
         Vector2 direction = new Vector2(inputMoviminetoHorizontal, inputMoviminetoVertical);
         Movimiento(direction);
+    }
+
+    protected void OnCollisionEnter2D(Collision2D colision)
+    {
+        if(colision.gameObject.tag == "Enemigo")
+        {
+            vida --;
+            if(vida == 0)
+            {
+                GameOver();
+            }
+        }
+    }
+
+    private void GameOver()
+    {
+        gameOverText.enabled = true;
+        Time.timeScale = 0;
     }
 }
