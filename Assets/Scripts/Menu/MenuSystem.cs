20c1d23bd022224f293794131201ebdf22822b0b
Santiago <santiagolozadafp23@ibf.cat>
2025-05-12 20:14:55 +0200

    modificaciones menus

--- Assets/Scripts/Menu/MenuSystem.cs
+++ Assets/Scripts/Menu/MenuSystem.cs
@@ -1,18 +1,45 @@
-using System;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuSystem : MonoBehaviour
 {
-    public void jugar() 
+    public GameObject panelPausa; 
+
+    private bool juegoPausado = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (juegoPausado)
+                Continuar();
+            else
+                Pausar();
+        }
+    }
+
+    public void jugar()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void salir() 
+    public void salir()
     {
         Debug.Log("Saliendo del juego");
         Application.Quit();
+    }
+
+    public void Pausar()
+    {
+        panelPausa.SetActive(true);
+        Time.timeScale = 0f;
+        juegoPausado = true;
+    }
+
+    public void Continuar()
+    {
+        panelPausa.SetActive(false);
+        Time.timeScale = 1f;
+        juegoPausado = false;
     }
 }
